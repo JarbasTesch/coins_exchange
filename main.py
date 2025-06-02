102511d5659347982be3b371df90bcc33c9a257c
JarbasTesch <jarbas.viana@fu2re.com.br>
2025-06-02 13:16:52 -0300

    att

--- main.py
+++ main.py
@@ -1,17 +1,61 @@
-print('atualizando o git')
-
-'''
-Precisa criar a venv!!!!
-'''
-
 import requests
 
-url = "https://api.exemplo.com/dados"
-response = requests.get(url)
+# #url = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
+# url= "https://economia.awesomeapi.com.br/json/daily/USD-BRL/15"
+# resposta = requests.get(url)
+#
+# # Verifica se a requisição foi bem-sucedida (código 200)
+# if resposta.status_code == 200:
+#     dados = resposta.json()
+#     print(dados)
+# else:
+#     print(f"Erro na requisição: {resposta.status_code}")
+#
 
-# Verifica se a requisição foi bem-sucedida (código 200)
-if response.status_code == 200:
-    dados = response.json()  # Converte a resposta para dicionário Python
-    print(dados)
-else:
-    print(f"Erro na requisição: {response.status_code}")
+def cotacao_euro():
+    url = "https://economia.awesomeapi.com.br/json/daily/EUR-BRL"
+    resposta = requests.get(url)
+
+    # Verifica se a requisição foi bem-sucedida (código 200)
+    if resposta.status_code == 200:
+        dados = resposta.json()
+        bid = dados[0]["bid"]
+        print(f"Cotação atual do Euro: R$ {float(bid):.2f}")
+    else:
+        print(f"Erro na requisição: {resposta.status_code}")
+
+def cotacao_dolar():
+    url = "https://economia.awesomeapi.com.br/json/daily/USD-BRL"
+    resposta = requests.get(url)
+
+    # Verifica se a requisição foi bem-sucedida (código 200)
+    if resposta.status_code == 200:
+        dados = resposta.json()
+        bid = dados[0]["bid"]
+        print(f"Cotação atual do Euro: R$ {float(bid):.2f}")
+    else:
+        print(f"Erro na requisição: {resposta.status_code}")
+
+def cotacao_moeda(moeda_real):
+    url = f"https://economia.awesomeapi.com.br/json/daily/{moeda_real}-BRL"
+    resposta = requests.get(url)
+
+    # Verifica se a requisição foi bem-sucedida (código 200)
+    if resposta.status_code == 200:
+        dados = resposta.json()
+        bid = dados[0]["bid"]
+        print(f"Cotação atual do Euro: R$ {float(bid):.2f}")
+    else:
+        print(f"Erro na requisição: {resposta.status_code}")
+
+moeda_escolhida = input(
+    '''Escolha a moeda digitando o número:
+    1) USD - Dólar
+    2) EUR - Euro
+    3) BTC - Bitcoin
+    '''
+)
+
+#FAZER O IF PRA SETAR A MOEDA
+
+cotacao_moeda(moeda_escolhida)
