e0e4a5f16bbc9f211f4f7d203b91780c05429419
JarbasTesch <jarbas.viana@fu2re.com.br>
2025-06-03 12:47:56 -0300

    att

--- main.py
+++ main.py
@@ -36,26 +36,38 @@ def cotacao_dolar():
     else:
         print(f"Erro na requisição: {resposta.status_code}")
 
-def cotacao_moeda(moeda_real):
-    url = f"https://economia.awesomeapi.com.br/json/daily/{moeda_real}-BRL"
+
+
+def cotacao_moeda(abreviacao, moeda):
+    url = f"https://economia.awesomeapi.com.br/json/daily/{abreviacao}-BRL"
     resposta = requests.get(url)
 
     # Verifica se a requisição foi bem-sucedida (código 200)
     if resposta.status_code == 200:
         dados = resposta.json()
         bid = dados[0]["bid"]
-        print(f"Cotação atual do Euro: R$ {float(bid):.2f}")
+        print(f"Cotação atual do {moeda}: R$ {float(bid):.2f}")
     else:
         print(f"Erro na requisição: {resposta.status_code}")
 
-moeda_escolhida = input(
-    '''Escolha a moeda digitando o número:
-    1) USD - Dólar
-    2) EUR - Euro
-    3) BTC - Bitcoin
-    '''
-)
 
-#FAZER O IF PRA SETAR A MOEDA
+def escolher_moeda():
+    moeda_escolhida = input(
+        '''Escolha a moeda digitando o número:
+        1) USD - Dólar
+        2) EUR - Euro
+        3) BTC - Bitcoin
+        '''
+    )
 
-cotacao_moeda(moeda_escolhida)
+    if moeda_escolhida == '1':
+        return 'USD', 'Dólar'
+    elif moeda_escolhida == '2':
+        return 'EUR', 'Euro'
+    elif moeda_escolhida == '3':
+        return 'BTC', 'Bitcoin'
+
+
+abreviacao_, moeda_ = escolher_moeda()
+
+cotacao_moeda(abreviacao_, moeda_)
